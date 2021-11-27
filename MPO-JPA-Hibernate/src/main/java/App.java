4f6a804b3e13aaeb0cd1cdaec695f7bd943f06d3
Andrea <andrea.gomez@alumno.iesluisvives.org>
2021-11-27 15:35:49 +0100

    Reestructuración proyecto, dividido en dos.

---
+++ MPO-JPA-Hibernate/src/main/java/App.java
@@ -0,0 +1,9 @@
+import repository.DepartamentoRepository;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("HOLA MPO JPA!!");
+
+
+    }
+}
---
+++ MPO-JPA-Hibernate/src/main/java/repository/DepartamentoRepository.java
@@ -0,0 +1,35 @@
+package repository;
+
+
+import dao.Departamento;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class DepartamentoRepository implements CrudRepository<Departamento, Long> {
+
+    @Override
+    public List<Departamento> findAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Departamento getById(Long aLong) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Departamento save(Departamento departamento) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Departamento update(Departamento departamento) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Departamento delete(Departamento departamento) throws SQLException {
+        return null;
+    }
+}
