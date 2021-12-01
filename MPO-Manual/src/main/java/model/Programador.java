3fe8648ad626ecdf3ab74b401a6a640779100e57
Andrea <andrea.gomez@alumno.iesluisvives.org>
2021-12-01 19:22:20 +0100

    Implementacion nuevos DTO y Model

--- MPO-Manual/src/main/java/model/Programador.java
+++ MPO-Manual/src/main/java/model/Programador.java
@@ -11,119 +11,11 @@ import java.util.UUID;
 @Builder
 @AllArgsConstructor
 public class Programador {
-<<<<<<< HEAD
-    private int id;
-    private String name;
-    private Date fechaAlta;
-    private int departamento;
-    private List<String> proyectos;
-    private List<String> issues;
-    private List<String> commits;
-    private List<String> tecnologiasUsadas;
-    private double salario;
-    private String password;
-
-    public Programador(int id, String name, Date fechaAlta, int departamento, List<String> proyectos, List<String> issues, List<String> commits, List<String> tecnologiasUsadas, double salario, String password) {
-        this.id = id;
-        this.name = name;
-        this.fechaAlta = fechaAlta;
-        this.departamento = departamento;
-        this.proyectos = proyectos;
-        this.issues = issues;
-        this.commits = commits;
-        this.tecnologiasUsadas = tecnologiasUsadas;
-        this.salario = salario;
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getFechaAlta() {
-        return fechaAlta;
-    }
-
-    public void setFechaAlta(Date fechaAlta) {
-        this.fechaAlta = fechaAlta;
-    }
-
-    public int getDepartamento() {
-        return departamento;
-    }
-
-    public void setDepartamento(int departamento) {
-        this.departamento = departamento;
-    }
-
-    public List<String> getProyectos() {
-        return proyectos;
-    }
-
-    public void setProyectos(List<String> proyectos) {
-        this.proyectos = proyectos;
-    }
-
-    public List<String> getIssues() {
-        return issues;
-    }
-
-    public void setIssues(List<String> issues) {
-        this.issues = issues;
-    }
-
-    public List<String> getCommits() {
-        return commits;
-    }
-
-    public void setCommits(List<String> commits) {
-        this.commits = commits;
-    }
-
-    public List<String> getTecnologiasUsadas() {
-        return tecnologiasUsadas;
-    }
-
-    public void setTecnologiasUsadas(List<String> tecnologiasUsadas) {
-        this.tecnologiasUsadas = tecnologiasUsadas;
-    }
-
-    public double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(double salario) {
-        this.salario = salario;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-=======
     private UUID id;
     private String nombre;
     private Date fechaAlta;
     private String contraseña;
     private Double salario;
     private UUID idDepartamento;
->>>>>>> develop
 
 }
-
-
--- MPO-Manual/src/main/java/model/Proyecto.java
+++ MPO-Manual/src/main/java/model/Proyecto.java
@@ -5,106 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.sql.Date;
-<<<<<<< HEAD
-=======
 import java.util.UUID;
->>>>>>> develop
 
-@Data
 @Builder
 @AllArgsConstructor
 public class Proyecto {
-<<<<<<< HEAD
-    private int id;
-    private String nombre;
-    private int idJefeProyecto;
-    private double presupuesto;
+    private UUID idProgramador;
+    private UUID idProyecto;
     private Date fechaInicio;
     private Date fechaFin;
-    private String tecnologias;
-    private int repositorio;
-
-    public Proyecto(String nombre, int idJefeProyecto, double presupuesto, Date fechaInicio, Date fechaFin, String tecnologias, int repositorio) {
-        this.nombre = nombre;
-        this.idJefeProyecto = idJefeProyecto;
-        this.presupuesto = presupuesto;
-        this.fechaInicio = fechaInicio;
-        this.fechaFin = fechaFin;
-        this.tecnologias = tecnologias;
-        this.repositorio = repositorio;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getIdJefeProyecto() {
-        return idJefeProyecto;
-    }
-
-    public void setIdJefeProyecto(int idJefeProyecto) {
-        this.idJefeProyecto = idJefeProyecto;
-    }
-
-    public double getPresupuesto() {
-        return presupuesto;
-    }
-
-    public void setPresupuesto(double presupuesto) {
-        this.presupuesto = presupuesto;
-    }
-
-    public Date getFechaInicio() {
-        return fechaInicio;
-    }
-
-    public void setFechaInicio(Date fechaInicio) {
-        this.fechaInicio = fechaInicio;
-    }
-
-    public Date getFechaFin() {
-        return fechaFin;
-    }
-
-    public void setFechaFin(Date fechaFin) {
-        this.fechaFin = fechaFin;
-    }
-
-    public String getTecnologias() {
-        return tecnologias;
-    }
-=======
-
-   private UUID idProgramador;
-   private UUID idProyecto;
-   private Date fechaInicio;
-   private Date fechaFin;
-
->>>>>>> develop
-
-
-<<<<<<< HEAD
-    public int getRepositorio() {
-        return repositorio;
-    }
-
-    public void setRepositorio(int repositorio) {
-        this.repositorio = repositorio;
-    }
-}
-=======
 
 }
->>>>>>> develop
